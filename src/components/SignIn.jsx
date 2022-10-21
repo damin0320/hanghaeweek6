ac2cb09eddd1982f8fdc95650c82b993621a6e19
damin <damin0320@gmail.com>
2022-10-21 22:43:09 +0900

    ADD : 기본 버튼 및 인풋 세팅

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 const SignIn = () => {
+  const navigate = useNavigate()
   return (
-    <div>SignIn</div>
+    <div>
+    <h1>로그인</h1>
+    <div><input placeholder='아이디'></input></div>
+    <div><input placeholder='비밀번호'></input></div>
+    <button onClick={() => {
+            navigate("/movielist");
+          }}>로그인</button>
+    <button onClick={() => {
+            navigate("/signup");
+          }}>회원가입</button>
+
+    </div>
   )
 }
 
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 const SignUp = () => {
+  const navigate = useNavigate()
   return (
-    <div>SignUp</div>
+    <div>    
+    <h1>회원가입</h1>
+    <div><input placeholder='아이디'></input> <button>중복확인</button></div>
+    <div><input placeholder='닉네임'></input> <button>중복확인</button></div>
+    <div><input placeholder='비밀번호'></input></div>
+    <div><input placeholder='비밀번호 확인'></input></div>
+    <div>    <button onClick={() => {
+            navigate("/movielist");
+          }}>회원가입</button>    <button onClick={() => {
+            navigate("/");
+          }}>뒤로가기</button></div>
+    </div>
   )
 }
 
