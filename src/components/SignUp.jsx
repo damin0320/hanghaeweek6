f7589869a876cb2711aa20c932d721c42195161f
damin <damin0320@gmail.com>
2022-10-22 22:52:44 +0900

    ADD : 회원가입 정규표현식 추가

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -32,6 +32,10 @@ const SignUp = () => {
     password2: join.password2,
   }
 
+const userIdCheck = /^[a-z]+[a-z0-9]{5,19}$/g;
+const usernicknameCheck = /^[a-z]+[a-z0-9]{5,19}$/g;
+const passwordCheck = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/;
+
 const onCheckUserId = () => {
   account.map((item) => {
     if(obj.userId === item.userId) {
@@ -54,6 +58,15 @@ const onCheckNicknameId = () => {
 
   const onSubmitHandler = (event) => {
     event.preventDefault()
+    if(!userIdCheck.test(obj.userId)){
+      return alert("아이디 양식에 맞춰주세요")
+    }
+    if(!usernicknameCheck.test(obj.nickname)){
+      return alert("닉네임 양식에 맞춰주세요")
+    }
+    if(!passwordCheck.test(obj.password)){
+      return alert("비밀번호 양식에 맞춰주세요")
+    }
     if(obj.password !== obj.password2){
       return alert("비밀번호를 모두 확인해주세요")
     }
@@ -75,10 +88,11 @@ const onCheckNicknameId = () => {
   return (
     <div>    
     <h1>회원가입</h1>
-    <div><input name='userId' placeholder='아이디' onChange={onChangeHandler}></input> <button type="button" onClick={onCheckUserId}>중복확인</button></div>
-    <div><input name='nickname' placeholder='닉네임' onChange={onChangeHandler}></input> <button type="button" onClick={onCheckNicknameId}>중복확인</button></div>
-    <div><input name='password' placeholder='비밀번호' onChange={onChangeHandler}></input></div>
-    <div><input name='password2' placeholder='비밀번호 확인' onChange={onChangeHandler}></input></div>
+    <div><input style={{width:300}} name='userId' onChange={onChangeHandler} placeholder="아이디는 영문자로 시작하는 영문자 또는 숫자 6~20자"></input> <button type="button" onClick={onCheckUserId}>중복확인</button></div>
+    <div><input style={{width:300}} name='nickname' onChange={onChangeHandler} placeholder="닉네임은 영문자로 시작하는 영문자 또는 숫자 6~20자"></input> <button type="button" onClick={onCheckNicknameId}>중복확인</button></div>
+    <div><input style={{width:300}} name='password' onChange={onChangeHandler} placeholder="비밀번호는 8 ~ 16자 영문, 숫자 조합
+"></input></div>
+    <div><input style={{width:300}} name='password2' placeholder='비밀번호 확인' onChange={onChangeHandler}></input></div>
     <div>    <button onClick={onSubmitHandler}>회원가입</button>    <button onClick={() => {
             navigate("/");
           }}>뒤로가기</button></div>
