a7e3b2d629e19797e09bf3bbaf51dd55c345d721
damin <damin0320@gmail.com>
2022-10-24 20:49:52 +0900

    ADD : 로그인 및 회원가입, 로그아웃 기능

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { __userSignUp, __userSignUpGet } from '../redux/modules/LoginSlice'
+import { __userSignUp, __checkId, __checkName } from '../redux/modules/LoginSlice'
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
@@ -8,15 +8,14 @@ import { useEffect } from 'react'
 const SignUp = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
-  const {account} = useSelector((state) => state.account)
-  useEffect(() => {dispatch(__userSignUpGet())
-  }, [dispatch])
+  const {account, idCheck, nameCheck} = useSelector((state) => state.account)
+
 
   const initialState = {
-    userId: "",
+    userid: "",
     nickname: "",
     password: "",
-    password2: "",    
+    passwordconfirm: "",    
   };
   const [join, setJoin] = useState(initialState);
   const onChangeHandler = (event) => {
@@ -26,39 +25,47 @@ const SignUp = () => {
   const obj = {
     id : 1,
     //임시
-    userId: join.userId,
+    userid: join.userid,
     nickname: join.nickname,
     password: join.password,
-    password2: join.password2,
+    passwordconfirm: join.passwordconfirm,
   }
 
-const userIdCheck = /^[a-z]+[a-z0-9]{5,19}$/g;
+const useridCheck = /^[a-z]+[a-z0-9]{5,19}$/g;
 const usernicknameCheck = /^[a-z]+[a-z0-9]{5,19}$/g;
 const passwordCheck = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/;
 
-const onCheckUserId = () => {
-  account.map((item) => {
-    if(obj.userId === item.userId) {
-      return alert("사용 중인 ID가 있습니다.")
-    }else{
-      return alert("사용 가능합니다.")
-    }
-  })
-  
+const onCheckId = () => {
+  // 수정 필요(true, false로만 받으면 됨. if 필요 없음. dispatch로 받으면 됨)
+  dispatch(__checkId(obj.userid))
 }
-const onCheckNicknameId = () => {
-  account.map((item) => {
-    if(obj.nickname === item.nickname) {
-      return alert("사용 중인 닉네임이 있습니다.")
+
+useEffect(() => {
+  if(idCheck !== undefined){
+    if(idCheck.success === true){
+      return alert("사용 가능한 ID입니다.")
     }else{
-      return alert("사용 가능합니다.")
+      return alert("이미 사용중인 ID가 있습니다.")
     }
-  })
+  }
+}, [dispatch, idCheck])
+const onCheckname = () => {
+  dispatch(__checkName(obj.nickname))
 }
+useEffect(() => {
+  if(nameCheck !== undefined){
+    if(nameCheck.success === true){
+      return alert("사용 가능한 닉네임입니다.")
+    }else{
+      return alert("이미 사용중인 닉네임이 있습니다.")
+    }
+  }
+}, [dispatch, nameCheck])
+
 
   const onSubmitHandler = (event) => {
     event.preventDefault()
-    if(!userIdCheck.test(obj.userId)){
+    if(!useridCheck.test(obj.userid)){
       return alert("아이디 양식에 맞춰주세요")
     }
     if(!usernicknameCheck.test(obj.nickname)){
@@ -67,32 +74,50 @@ const onCheckNicknameId = () => {
     if(!passwordCheck.test(obj.password)){
       return alert("비밀번호 양식에 맞춰주세요")
     }
-    if(obj.password !== obj.password2){
+    if(obj.password !== obj.passwordconfirm){
       return alert("비밀번호를 모두 확인해주세요")
     }
-    if(obj.password === "" || obj.password2 === "" || obj.password === undefined || obj.password2 === undefined){
+    if(obj.password === "" || obj.passwordconfirm === "" || obj.password === undefined || obj.passwordconfirm === undefined){
       return alert("빈칸을 입력해주세요.")
     }
-    if(obj.userId === "" || obj.userId === undefined) {
+    if(obj.userid === "" || obj.userid === undefined) {
       return alert("빈칸을 입력해주세요.")
     }
 
     if(obj.nickname === "" || obj.nickname === undefined) {
       return alert("빈칸을 입력해주세요.")
     }
+
     dispatch(__userSignUp(obj))
-    alert("회원가입이 완료되었습니다.")
-    navigate("/");
+
   }
+  useEffect(() => {
+    if(account !== undefined){
+    if(account.success === true){
+      alert("회원가입이 완료되었습니다.")
+      setJoin({
+        userid: "",
+        nickname: "",
+        password: "",
+        passwordconfirm: "",  
+      })
+        window.location.replace("/")
+    }else{
+      if(account.error !== undefined){
+        alert(account.error)
+      }
+     }
+  }
+  }, [account])
 
   return (
     <div>    
     <h1>회원가입</h1>
-    <div><input style={{width:300}} name='userId' onChange={onChangeHandler} placeholder="아이디는 영문자로 시작하는 영문자 또는 숫자 6~20자"></input> <button type="button" onClick={onCheckUserId}>중복확인</button></div>
-    <div><input style={{width:300}} name='nickname' onChange={onChangeHandler} placeholder="닉네임은 영문자로 시작하는 영문자 또는 숫자 6~20자"></input> <button type="button" onClick={onCheckNicknameId}>중복확인</button></div>
+    <div><input style={{width:300}} name='userid' onChange={onChangeHandler} placeholder="아이디는 영문자로 시작하는 영문자 또는 숫자 6~20자"></input> <button type="button" onClick={onCheckId}>중복확인</button></div>
+    <div><input style={{width:300}} name='nickname' onChange={onChangeHandler} placeholder="닉네임은 영문자로 시작하는 영문자 또는 숫자 6~20자"></input> <button type="button" onClick={onCheckname}>중복확인</button></div>
     <div><input style={{width:300}} name='password' onChange={onChangeHandler} placeholder="비밀번호는 8 ~ 16자 영문, 숫자 조합
 "></input></div>
-    <div><input style={{width:300}} name='password2' placeholder='비밀번호 확인' onChange={onChangeHandler}></input></div>
+    <div><input style={{width:300}} name='passwordconfirm' placeholder='비밀번호 확인' onChange={onChangeHandler}></input></div>
     <div>    <button onClick={onSubmitHandler}>회원가입</button>    <button onClick={() => {
             navigate("/");
           }}>뒤로가기</button></div>
--- src/redux/modules/LoginSlice.js
+++ src/redux/modules/LoginSlice.js
@@ -13,15 +13,16 @@ export const __userLogin = createAsyncThunk(
   // login : reducer name, 경로 정해줘야
   async (payload, thunkAPI) => {
     try {
-      const data = await axios.post("http://localhost:3001/account", payload);
-      const accessToken = data.headers.authorization;
-      const refreshToken = data.headers["refresh-token"];
+      const data = await axios.post("http://52.206.73.25:8080/account/login", payload);
+      const Access_Token = data.headers.access_token;
+      // const refreshToken = data.headers["refresh-token"];
       if (data.status === 200 || data.status === 201) {
-        window.localStorage.setItem("accessToken", accessToken);
-        window.localStorage.setItem("refreshToken", refreshToken);
-        window.localStorage.setItem("nickname", data.data.data.nickname);
+        window.localStorage.setItem("Access_Token", Access_Token);
+        // window.localStorage.setItem("refreshToken", refreshToken);
+        window.localStorage.setItem("nickname", data.data.data);
         alert("로그인 성공");
-        window.location.replace("/")}
+        window.location.replace("/movielist")
+      }
 
       return thunkAPI.fulfillWithValue(payload)
     } catch (error) {
@@ -34,11 +35,25 @@ export const __userLogin = createAsyncThunk(
   }
 );
 
-export const  __userSignUpGet = createAsyncThunk(
-  "account/userSignUpGet",
+export const  __checkId = createAsyncThunk(
+  "account/checkId",
+  // type
   async (payload, thunkAPI) => {
     try {
-      const data = await axios.get("http://localhost:3001/account")
+    const data = await axios.post("http://52.206.73.25:8080/account/checkid", {userid: payload})
+      return thunkAPI.fulfillWithValue(data.data)
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error)
+    }
+  }
+)
+
+export const  __checkName = createAsyncThunk(
+  "account/checkName",
+  async (payload, thunkAPI) => {
+    try {
+      const data = await axios.post("http://52.206.73.25:8080/account/checkname", {nickname: payload})
+      // 415는 타입에러. {}로 감싸서 보낸다.
       return thunkAPI.fulfillWithValue(data.data)
     } catch (error) {
       return thunkAPI.rejectWithValue(error)
@@ -50,8 +65,8 @@ export const  __userSignUp = createAsyncThunk(
   "account/userSignUp",
   async (payload, thunkAPI) => {
     try {
-      const data = await axios.post("http://localhost:3001/account", payload)
-      return thunkAPI.fulfillWithValue(payload)
+      const data = await axios.post("http://52.206.73.25:8080/account/signup", payload)
+      return thunkAPI.fulfillWithValue(data.data)
     } catch (error) {
       return thunkAPI.rejectWithValue(error)
     }
@@ -77,15 +92,28 @@ export const LoginSlice = createSlice({
       state.isSuccess = false;
       state.error = action.payload; // catch 된 error 객체를 state.error에 넣습니다.
     },
-    [__userSignUpGet.pending]: (state) => {
+    [__checkId.pending]: (state) => {
       state.isLoading = true; // 네트워크 요청이 시작되면 로딩상태를 true로 변경합니다.
     },
-    [__userSignUpGet.fulfilled]: (state, action) => {
+    [__checkId.fulfilled]: (state, action) => {
       state.isLoading = false; // 네트워크 요청이 끝났으니, false로 변경합니다.
       state.isSuccess = false;
-      state.account=action.payload;
+      state.idCheck=action.payload;
     },
-    [__userSignUpGet.rejected]: (state, action) => {
+    [__checkId.rejected]: (state, action) => {
+      state.isLoading = false; // 에러가 발생했지만, 네트워크 요청이 끝났으니, false로 변경합니다.
+      state.isSuccess = false;
+      state.error = action.payload; // catch 된 error 객체를 state.error에 넣습니다.
+    },
+    [__checkName.pending]: (state) => {
+      state.isLoading = true; // 네트워크 요청이 시작되면 로딩상태를 true로 변경합니다.
+    },
+    [__checkName.fulfilled]: (state, action) => {
+      state.isLoading = false; // 네트워크 요청이 끝났으니, false로 변경합니다.
+      state.isSuccess = false;
+      state.nameCheck=action.payload;
+    },
+    [__checkName.rejected]: (state, action) => {
       state.isLoading = false; // 에러가 발생했지만, 네트워크 요청이 끝났으니, false로 변경합니다.
       state.isSuccess = false;
       state.error = action.payload; // catch 된 error 객체를 state.error에 넣습니다.
@@ -96,7 +124,7 @@ export const LoginSlice = createSlice({
     [__userSignUp.fulfilled]: (state, action) => {
       state.isLoading = false; // 네트워크 요청이 끝났으니, false로 변경합니다.
       state.isSuccess = false;
-      state.account.push(action.payload); // 
+      state.account=action.payload; // 
     },
     [__userSignUp.rejected]: (state, action) => {
       state.isLoading = false; // 에러가 발생했지만, 네트워크 요청이 끝났으니, false로 변경합니다.
